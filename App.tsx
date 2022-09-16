296114cc6c6f5246b486ab87bb567aae043c5c9d
samuckqadev <samuel.tomimatu@bandtec.com.br>
2022-09-16 00:28:54 -0300

    The Start

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text >Tomimatus ! </Text>
+      <Button title={'Click me'} />
       <StatusBar style="auto" />
     </View>
   );
 }
 
+type ButtonProps = {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity >
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: 'red',
+    color: 'white',
+  }
+
 });
